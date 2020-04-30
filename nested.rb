050d42c928c8b37ec6cf8b299d4428ee4fdd5db2
Jung Lee <lee.jung.s95@gmail.com>
2020-04-30 05:22:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -52,7 +52,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
 	puts programmer_hash[:dennis_ritchie][:languages]
